74c3c497147fa36ea3aa7d23b1b4e0cc321a3b6c
roger <perezmenarogelio@gmail.com>
2022-06-29 13:17:07 -0500

    Auth - Login/Register

---
+++ README.md
@@ -0,0 +1,8 @@
+# Share Me
+Social media fullstack project app.
+
+## Frontend
+Aplication is built with [React](https://reactjs.org/).
+
+## Backend
+Aplication is built with [Sanity.io](https://www.sanity.io/).
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,23 @@
-import React from 'react'
+import React, {useEffect} from 'react'
 import { Routes, Route, useNavigate } from 'react-router-dom'
+import { gapi } from 'gapi-script'
 
 import Login from './components/Login'
 import Home from './container/Home'
 
 const App = () => {
+
+
+  useEffect(() => {
+    gapi.load('client:auth2', () => {
+      gapi.client.init({
+        clientId: process.env.REACT_APP_GOOGLE_API_TOKEN,
+        scope: 'profile email'
+      })
+    }
+    )
+  })
+  
   return (
     <Routes>
       <Route path='login' element={<Login />} />
