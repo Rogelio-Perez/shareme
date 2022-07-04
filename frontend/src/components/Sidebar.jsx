245bfaab5413e097d782ffd78133fb8eb6655c74
roger <perezmenarogelio@gmail.com>
2022-07-04 17:40:22 -0500

    User profile and Fix user-profile route

--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -4,18 +4,10 @@ import { RiHomeFill } from 'react-icons/ri'
 import { IoIosArrowForward } from 'react-icons/io'
 
 import logo from '../assets/logo.png'
+import { categories } from '../utils/data'
 
 const isNotActiveStyle = 'flex items-center px-5 gap-3 text-gray-500 hover:text-black transition-all duration-200 ease-in-out capitalize'
 const isActiveStyle = 'flex items-center px-5 gap-3 font-extrabold border-r-2 border-black 500 transition-all duration-200 ease-in-out capitalize'
-
-const categories = [
-  { name: 'Animals'},
-  { name: 'Wallpapers'},
-  { name: 'Photography'},
-  { name: 'Gaming'},
-  { name: 'Coding'},
-  { name: 'Other'},
-]
 
 const Sidebar = ({ user, closeToggle }) => {
   const handleCloseSidebar = () => {
@@ -50,6 +42,7 @@ const Sidebar = ({ user, closeToggle }) => {
               onClick={handleCloseSidebar}
               key={category.name}
             >
+              <img src={category.image} className='w-8 h-8 rounded-full shadow-sm' alt='category' />
               {category.name}
             </NavLink>
           ))}
@@ -57,7 +50,7 @@ const Sidebar = ({ user, closeToggle }) => {
       </div>
       {user && (
         <Link
-          to={`/user/profile/${user._id}`}
+          to={`/user-profile/${user._id}`}
           className='flex my-5 mb-3 gap-2 p-2 items-center bg-white rounded-lg shadow-lg mx-3'
           onClick={handleCloseSidebar}
         >
